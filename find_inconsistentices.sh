f127ac353b58b65037918d6a57ac3284b7257505
joncrall <crallj@rpi.edu>
2017-05-25 09:03:57 -0400

    Edits to first part of chapter 5

---
+++ find_inconsistentices.sh
@@ -0,0 +1,20 @@
+# We want all percents less than 1 to be prefixed with a 0.
+grep -ER "\\$\\.[0-9]*\\\\percent" *
+grep -ER "0\.[0-9]*\\\\percent" *
+
+grep -ER "\\$\\.[0-9]*\\$" *
+
+
+grep -Przo "(?s)Fisher *. *Vector" *.tex
+grep -Przo "(?s)Hamming *. *Em" *.tex
+grep -Przo "(?s)Selective *. *Match" *.tex
+grep -Przo "(?s)Vector *. *of *. *Locally" *.tex
+grep -Przo "(?s)invariant *. *feature" *.tex
+grep -Przo "(?s)Invariant *. *Feature" *.tex
+
+grep -Er "L2" *.tex
+grep -Er "L1" *.tex
+
+Gradient Location-Orientation Histogram
+Gradient Location-Orientation Histogram
+Speeded Up Robust Features
